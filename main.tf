8b9810b7fbf611a3018a42613cb66e19c67ec543
Jonathan Sabo <jonathan@sabo.io>
2024-09-03 08:43:49 -0400

    wip

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "hcloud_network" "private_network" {
+  name     = "${var.prefix}-network"
+  ip_range = var.network_ip_range
+}
+
+resource "hcloud_network_subnet" "private_network_subnet" {
+  type         = "cloud"
+  network_id   = hcloud_network.private_network.id
+  network_zone = var.network_zone
+  ip_range     = var.subnet_ip_range
+}
+
+resource "hcloud_ssh_key" "default" {
+  name       = "default ssh key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "hcloud_server" "node" {
+  count       = var.server_count
+  name        = "${var.prefix}-node-${count.index + 1}"
+  image       = var.server_image
+  server_type = var.server_type
+
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+
+  network {
+    network_id = hcloud_network.private_network.id
+    ip         = cidrhost(hcloud_network_subnet.private_network_subnet.ip_range, count.index + 11)
+  }
+
+  ssh_keys = [hcloud_ssh_key.default.name]
+
+  user_data = file("${path.module}/cloud-init.yaml")
+
+  depends_on = [hcloud_network_subnet.private_network_subnet]
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "hcloud_token" {
+  description = "Hetzner Cloud API token."
+  type        = string
+  sensitive   = true
+  default     = "" # Default to empty, will use environment TF_VAR_hcloud_token variable if set
+}
+
+variable "prefix" {
+  description = "Prefix for all resources."
+  type        = string
+  default     = "neu"
+}
+
+variable "network_ip_range" {
+  description = "The IP range for the Hetzner Cloud network."
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_ip_range" {
+  description = "The IP range for the Hetzner Cloud network subnet."
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "network_zone" {
+  description = "The Hetzner Cloud network zone for the subnet."
+  type        = string
+  default     = "eu-central"
+}
+
+variable "server_count" {
+  description = "The number of servers to create."
+  type        = number
+  default     = 3
+}
+
+variable "server_image" {
+  description = "The image to use for the servers."
+  type        = string
+  default     = "ubuntu-24.04"
+}
+
+variable "server_type" {
+  description = "The type of server to create."
+  type        = string
+  default     = "cax11"
+}
